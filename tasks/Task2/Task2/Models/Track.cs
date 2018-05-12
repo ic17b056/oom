d9a04a608333ae4c8e7466dbdafd57d2a92f0bc1
ic17b056 <ic17b056@technikum-wien.at>
2018-05-12 16:18:14 +0200

    add project Task2

---
+++ tasks/Task2/Task2/Models/Track.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2.Models
+{
+    class Track
+    {
+        private string title;
+        private double duration;
+        private string artist;
+
+        public Track(string title, double duration, string artist)
+        {
+            Title = title;
+            Duration = duration;
+            UpdateArtist(artist);
+        }
+
+        public void UpdateArtist(string artist) => this.artist = artist;
+
+        public double Duration {
+            get
+            {
+                return this.duration;
+            }
+            set
+            {
+                // the keyword 'value' represents the value that is assigned
+                if (value < 0) throw new Exception("Duration must not be negative.");
+                this.duration = value;
+            }
+        }
+
+        public string Title
+        {
+            get
+            {
+                return this.title;
+            }
+            set
+            {
+                // the keyword 'value' represents the value that is assigned
+                if (value == null && value.Length == 0) throw new Exception("Title must not be empty.");
+                this.title = value;
+            }
+        }
+
+        public string Artist
+        {
+            get
+            {
+                return this.artist;
+            }
+        }
+
+    }
+
+    
+}
---
+++ tasks/Task2/Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task2.Models;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Track track1 = new Track("FirstTrack", 2.5, "FirstArtist");
+            Console.WriteLine("Constructor Values Title: " + track1.Title + " Duration: " + track1.Duration + " Artist: " + track1.Artist);
+            track1.UpdateArtist("NewArtist");
+            Console.WriteLine("After UpdateArtist Values Title: " + track1.Title + " Duration: " + track1.Duration + " Artist: " + track1.Artist);
+
+        }
+    }
+}
